b423303ace6d25d40e9d555f73b369ba87eca64a
Hersh Thakur <hersh.thakur@gmail.com>
2023-12-06 13:38:04 +0000

    adds delete button for contacts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,22 @@ import Link from "next/link";
 
 import { CreateContact } from "@/app/_components/createContact";
 import { api } from "@/trpc/server";
+import { type NextPage } from "next";
+import ContactCard from "./_components/ContactCard";
 
-export default async function Home() {
+const Home: NextPage = async () => {
   const { contacts } = await api.contact.getAll.query();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <CreateContact />
       <div className="mt-10 flex flex-col gap-4">
         {contacts.map((contact) => (
-          <div key={contact.id} className="flex flex-col gap-2">
-            <div>{contact.address}</div>
-            <div>{contact.phone}</div>
-          </div>
+          <ContactCard key={contact.id} contact={contact} />
         ))}
       </div>
     </main>
   );
-}
+};
+
+export default Home;
